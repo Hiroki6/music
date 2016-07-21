23d5c29c0c62eeb6bfb729091292b1376c579d2e
fuzyco <bkn4xuimp.gvr2kg.e@gmail.com>
2016-07-21 21:26:51 +0900

    add css pass

--- music/urls.py
+++ music/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'recommendation/login.html'}),
@@ -22,3 +23,5 @@ urlpatterns = [
     url(r'^recommendation/', include('recommendation.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
