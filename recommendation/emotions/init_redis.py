45ba217e64a6fbf600f579e02d3b7a16b0eb7623
fuzyco <bkn4xuimp.gvr2kg.e@gmail.com>
2016-09-14 00:14:27 +0900

    define init_redis

---
+++ recommendation/emotions/init_redis.py
@@ -0,0 +1,23 @@
+# -*- coding:utf^8 -*-
+
+HOST = "localhost"
+PORT = 6379
+DB = 2
+
+"""
+モデルの初期化(各ユーザーごとのモデル配列の作成・保存)
+"""
+def init_model():
+    return
+
+"""
+モデルのredisへの保存
+"""
+def save_redis():
+    return
+
+"""
+モデルの作成
+"""
+def create_model():
+    return
